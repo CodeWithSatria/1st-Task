72b9ee47e93ba19b5c4b80c516381567c7a0b8a0
CodeWithSatria <CodeWithSatria@gmail.com>
2024-02-29 22:13:46 +0800

    changes

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import './globals.css'
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+import Introduction from "@/components/Introduction"
+import Banner from '@/components/Banner'
+import Projects from '@/components/Projects'
+import Script from 'next/script'
+
+export const metadata = {
+  title: 'Satria CV',
+  description: 'My Personal CV',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body>
+        <Navbar />
+        <main>
+          {children}
+          <Introduction />
+          <Banner />
+          <Projects />
+        </main>
+        <Footer/>
+        <Script src="index.js" strategy="beforeInteractive" />
+      </body>
+    </html>
+  )
+}
